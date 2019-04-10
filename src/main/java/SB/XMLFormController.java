39e87e48f37a2e292f0d7f75f145a622712f8a8c
stasadd <stas.add@gmail.com>
2019-04-10 20:31:30 +0300

    First Commit

---
+++ src/main/java/SB/XMLFormController.java
@@ -0,0 +1,60 @@
+package SB;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class XMLFormController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label TextName1;
+
+    @FXML
+    private Label TextName2;
+
+    @FXML
+    private Label TextName3;
+
+    @FXML
+    private Label TextName4;
+
+    @FXML
+    private Label TextName6;
+
+    @FXML
+    private Label TextName5;
+
+    @FXML
+    private JFXButton ButtonAlarm;
+
+    @FXML
+    private JFXButton ButtonWashing;
+
+    @FXML
+    private JFXButton ButtonCoffe;
+
+    @FXML
+    private JFXButton ButtonRefrigerator;
+
+    @FXML
+    private JFXButton ButtonStove;
+
+    @FXML
+    private JFXButton ButtonСonditioner;
+
+    @FXML
+    private JFXButton ButtonTurnoff;
+
+    @FXML
+    void initialize() {
+
+    }
+}

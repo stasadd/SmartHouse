2bd4d1114c1e52364d26588b914dbe45754a9a46
stasadd <stas.add@gmail.com>
2019-04-10 21:14:04 +0300

    Add Actions

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -13,8 +13,8 @@ public class Start extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/MainWindowFXML.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setWidth(340);
-        primaryStage.setHeight(320);
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
         primaryStage.setTitle("Login Seller");
         primaryStage.show();
     }
